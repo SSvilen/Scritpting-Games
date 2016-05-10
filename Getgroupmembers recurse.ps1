095923fa6859b53325a712bb622784585719a457
SSvilen <sultanov_s@yahoo.de>
2016-05-11 00:16:14 +0200

    Add files via upload

---
+++ Getgroupmembers recurse.ps1
@@ -0,0 +1,3 @@
+﻿$group = Read-Host 'Group Name'
+(([adsisearcher]"memberOf:1.2.840.113556.1.4.1941:=$(([adsisearcher]"name=$group").Findone().Path.Substring(7))").findall() `
+ | ? {$_.Properties.objectclass -contains "user"}).Properties.cn
